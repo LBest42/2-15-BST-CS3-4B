4252eb2cb4851cb432e9de861ca08c338ab608a6
gavin763 <125513459+gavin763@users.noreply.github.com>
2023-02-15 16:11:02 -0600

    Add files via upload

---
+++ PHamGavinAnimalBST.java
@@ -0,0 +1,109 @@
+import java.util.*;
+public class PHamGavinAnimalBST
+{
+    private AnimalNode root;
+    private class AnimalNode
+    {
+        String name;
+        AnimalNode left, right;
+        
+        public AnimalNode(String name)
+        {
+            this.name = name.toLowerCase();
+            left = null;
+            right = null;
+        }
+        public String toString()
+        {
+            return "" + name;
+        }
+    }
+    
+    public void insert(String n,AnimalNode hold, AnimalNode previous)
+    {
+        if(hold.left == null && hold.right == null)
+        {
+            hold = new AnimalNode(n);
+            return;
+        }
+        else
+        {
+            if(n.compareTo(hold.name) < 0)//if the string is less than the hold name, go left
+            {
+                if(hold.left == null)//if hold.left is null set it to a new node
+                {
+                    hold.left = new AnimalNode(n);
+                    return;
+                }
+                else
+                {
+                    insert(n, hold.left, hold);
+                }
+            }
+            else if(n.compareTo(hold.name) > 0)//if the string is more than the hold, go right
+            {
+                if(hold.right == null)
+                {
+                    hold.right = new AnimalNode(n);
+                    return;
+                }
+                else
+                {
+                    insert(n, hold.right, hold);
+                }
+            }
+        }
+    }
+    
+    public void delete(AnimalNode del, AnimalNode hold, AnimalNode prev)
+    {
+        if(hold.left == null && hold.right == null)
+        {
+            return;
+        }
+        else
+        {
+            if(hold.name.equalsIgnoreCase(del.name))//if it finds the node to delete
+            {
+                hold = null;
+                //nice
+            }
+            else
+            {
+                if(del.name.compareTo(hold.name) < 0)//if its smaller
+                {
+                    if(hold.left != null)
+                    {
+                        delete(del, hold.left, hold);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else if(del.name.compareTo(hold.name) > 0)//if its larger
+                {
+                    if(hold.right != null)
+                    {
+                        delete(del, hold.right, hold);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    
+    public String toString(String build)
+    {
+        return "hi";
+    }
+    
+    public static void main(String[] args)
+    {
+        PHamGavinAnimalBST bst = new PHamGavinAnimalBST();
+        
+    }
+}
